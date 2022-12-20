d840674240623c5b7f2420dec6cfeadba0723a44
Anuj Singla <asingla@redhat.com>
2022-12-20 14:20:23 +0530

    code changes .

---
+++ keypoints.ts
@@ -0,0 +1,68 @@
+/*
+Web accessbility
+
+Web accessibility means anybody with disabilities or without disabilities
+can access the web application. Web is an increasingly important resource
+in many aspects of life: government information, education resources,
+news or many more. So we should make web accessible to provide equal acess
+and equal opportunity to people with disabilities.
+
+- web access with disabilities images 
+https://www.google.com/search?q=web+access+with+disabilities&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiyx6qDvIP8AhW41nMBHUZeBOAQ_AUoAXoECAEQAw&biw=1536&bih=758&dpr=1.25
+
+First step to understand how people with disabilities use the web.
+How blind people use screen reader to interact with web.
+How people use joystick to interact with web
+Mouth stick
+Some people use text-to-speech app
+
+some people think accessbility is just a checklist.
+Digital information be accessible and well coded.
+
+Important points:
+- Page title is very important because we have multiple tabs open. It helps 
+to understand where on the web they are.
+
+- Properly heading structure (h1, h2, h3 - h6) of the page. It helps to
+understand the format of the information.
+
+- Link text is very important. We should not use link with ("learn more", "click here")
+or shortcut text.
+We use like this "Signup for the family plan" "more information for family plan"
+
+- Alt text important for image to understand it.
+
+- List items properly align.
+
+- Captions makes video accessible
+
+- Provide proper space, font size, color contrast.
+
+- Web focus is clearnly visible like button focus, link focus.
+
+How blind and deaf users access web: Digital Braille display
+https://www.youtube.com/watch?v=tVuLGrab9JA
+https://en.wikipedia.org/wiki/Refreshable_braille_display
+
+Screens Reader example - https://www.youtube.com/watch?v=dEbl5jvLKGQ
+
+- Deaf and sighted,low hearing, hard to hear disability users: 
+Captions and Transcripts for video and audio information
+
+- Vision decline due to aging (Parents) - People zoom to read the text.
+
+- Screen magnification to navigate the web.
+https://www.youtube.com/watch?v=4ZRVDgeMpXc
+
+International Standard for web accessibility
+
+4 Principles:
+
+- Perceivable
+- Operable
+- Understandable
+- Robust
+
+ */
+
+export {};
